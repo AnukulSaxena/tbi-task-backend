7856af6a3061f51cd7492df59793b79263273f11
Anukul Prakash Saxena <anukulsaxena83@gmail.com>
2024-03-31 00:43:16 +0530

    refactor: user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,11 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-import {
-  deleteFileOnCloudinary,
-  uploadOnCloudinary,
-} from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { email, username, password } = req.body;
+
   const existedUser = await User.findOne({
     $or: [{ email }, { username }],
   });
@@ -17,27 +14,10 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, "User with email or username already existed");
   }
 
-  if (!req?.files?.avatar) {
-    throw new ApiError(400, "Avatar file is required");
-  }
-  const avatarLocalPath = req?.files?.avatar[0]?.path;
-
-  if (!avatarLocalPath && !req.files.avatar[0].mimetype.includes("image")) {
-    throw new ApiError(400, "Avatar file is required or Invalid file Type");
-  }
-
-  const avatar = await uploadOnCloudinary(avatarLocalPath);
-  console.log(avatar);
-
-  if (!avatar) {
-    throw new ApiError(400, "Avatar file is required");
-  }
-
   const user = await User.create({
     username: username.toLowerCase(),
     email,
     password,
-    avatar: avatar.url,
   });
 
   const userCreated = await User.findById(user._id).select("-password");
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,19 +1,8 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
 import { userRegisterValidator } from "../validators/auth/user.validators.js";
 import { validate } from "../validators/validate.js";
 const router = Router();
 
-router.route("/register").post(
-  upload.fields([
-    {
-      name: "avatar",
-      maxCount: 1,
-    },
-  ]),
-  userRegisterValidator(),
-  validate,
-  registerUser
-);
+router.route("/register").post(userRegisterValidator(), validate, registerUser);
 export default router;

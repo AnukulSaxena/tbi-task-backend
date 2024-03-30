958cb0855d55bf994362bd5075b340e4d406ed6d
Anukul Prakash Saxena <anukulsaxena83@gmail.com>
2024-03-31 00:53:29 +0530

    fix: user validator

--- src/validators/auth/user.validators.js
+++ src/validators/auth/user.validators.js
@@ -8,14 +8,7 @@ const userRegisterValidator = () => {
       .withMessage("Email is Required")
       .isEmail()
       .withMessage("Invalid Email"),
-    body("username")
-      .trim()
-      .notEmpty()
-      .withMessage("Username is required")
-      .isLowercase()
-      .withMessage("Username must be lowercase")
-      .isLength({ min: 3 })
-      .withMessage("Username must be at lease 3 characters long"),
+    body("username").trim().notEmpty().withMessage("Username is required"),
     body("password").trim().notEmpty().withMessage("Password is required"),
   ];
 };
